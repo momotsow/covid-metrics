cf171ed4f6d611a2b6fa651147c09b963712fb5a
momotsow <kgomotsomkansi@gmail.com>
2023-01-06 19:24:26 +0200

    Add not found page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import './App.css';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+// import NotFound from './components/NotFound';
+// import Missions from './pages/Missions';
+// import Profile from './pages/Profile';
+// import Rockets from './pages/Rockets';
 
 function App() {
   return (
-    <div className="App">
-      <h1>hello</h1>
-    </div>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Rockets />} />
+        <Route path="/Missions" element={<Missions />} />
+        <Route path="/MyProfile" element={<Profile />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
   );
 }
 
