eb28b307ea036fd453c6c801c7332d4eec609025
momotsow <kgomotsomkansi@gmail.com>
2023-01-07 16:14:28 +0200

    Fix Linter issues

--- src/components/DetailsPage.js
+++ src/components/DetailsPage.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
-import fetchData from '../redux/covid/Api';
+import fetchData from '../redux/covid/api';
 import { fetchStats } from '../redux/covid/reducer';
 
 const DetailPage = () => {
